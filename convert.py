33fdfe8f5a2407efc13221ff768eedb3a7de598c
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-28 19:04:38 +0200

    added the scripts

--- convert.py
+++ convert.py
@@ -1,17 +1,15 @@
 from singularity_runner.sinexecuter import *
 import os  
 
-class format_converter(object):
-    def __init__(self, picture, work_dir):
-       self.picture = picture
-       self.format = picture.split(".")[-1]
+class FormatConverter(object):
+    def __init__(self):
        self.image_url = 'shub://stephansmit/inkscape_containers:latest'
        self.image_name = 'inkscape_containers_latest.sif'
-       self.image_dir = os.path.join(os.path.realpath(__file__), 'images')
+       self.image_dir = os.path.dirname(os.path.realpath(__file__))
 
     def convert(self,inputf_n, outputf_n, bind_dir):
-       runner = Runner(self.image_name, self.image_url, self.image_dir)
-       cmd = ['inkscape' '--file' ,inputf, '--export-emf', outputf]
+       runner = SinExecuter(self.image_name, self.image_url, self.image_dir)
+       cmd = ['inkscape','--file' ,inputf_n, '--export-emf', outputf_n]
        runner.run(cmd,bind_dir)
 
 
