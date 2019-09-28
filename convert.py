b6710826900cd2ba6bfbab020859d6c3dd48a7ee
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-28 19:26:51 +0200

    updated

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ class FormatConverter(object):
        self.image_name = 'inkscape_containers_latest.sif'
        self.image_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
 
-    def convert(self,inputf_n, outputf_n, bind_dir):
+    def convert_to_emf(self,inputf_n, outputf_n, bind_dir):
        runner = SinExecuter(self.image_name, self.image_url, self.image_dir)
        cmd = ['inkscape','--file' ,inputf_n, '--export-emf', outputf_n]
        runner.run(cmd,bind_dir)
