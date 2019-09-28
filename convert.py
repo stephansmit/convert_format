1381967028a5dd70618de393ec7e069bfca19186
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-28 19:09:56 +0200

    added the latest

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ class FormatConverter(object):
     def __init__(self):
        self.image_url = 'shub://stephansmit/inkscape_containers:latest'
        self.image_name = 'inkscape_containers_latest.sif'
-       self.image_dir = os.path.dirname(os.path.realpath(__file__))
+       self.image_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
 
     def convert(self,inputf_n, outputf_n, bind_dir):
        runner = SinExecuter(self.image_name, self.image_url, self.image_dir)
