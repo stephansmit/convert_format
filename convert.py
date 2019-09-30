78b7295200b7bb4b446fbee4a387bc4a5e6d993f
stephan <stephansmit@hotmail.com>
2019-09-30 13:39:59 +0200

    added the fixed

--- convert.py
+++ convert.py
@@ -11,9 +11,9 @@ class FormatConverter(object):
        self.image_name = 'inkscape_containers_latest.sif'
        self.image_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
 
-    def convert_to_emf(self,inputf_n, outputf_n, bind_dir):
+    def convert_to_wmf(self,inputf_n, outputf_n, bind_dir):
        runner = SinExecuter(self.image_name, self.image_url, self.image_dir)
-       cmd = ['inkscape','--file' ,inputf_n, '--export-emf', outputf_n]
+       cmd = ['inkscape','--file' ,inputf_n, '--export-wmf', outputf_n]
        runner.run(cmd,bind_dir)
 
 
